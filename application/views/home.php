32aa81dfb1832eaa433cdb191d10e4e4695d8fd3
aisyahnan <48095807+aisyahnan@users.noreply.github.com>
2019-05-12 22:28:10 +0700

    Add files via upload

--- application/views/home.php
+++ application/views/home.php
@@ -1,99 +1,106 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Kereta Join</title>
+    <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        .th{
+            text-align: center;
+            background-color: #e2e0e0;
+        }
+    </style>
 </head>
 <body>
-    <h2 align="center"> CI JOIN </h2>
-    <br><br>
-    <h4>LEFT JOIN LOKOMOTIF DAN KURSI</h4>
-    <hr>
-    <table border="1" cellspacing="0" cellpadding="8">
-        <thead>
-            <th>kd_kereta(L)</th>
-            <th>nama(L)</th>
-            <th>kd_kursi(K)</th>
-            <th>kd_gerbong(K)</th>
-            <th>kd_kereta(K)</th>
-        </thead>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <a class="navbar-brand" href="<?php echo site_url("CForm/index") ?>">Kereta Join</a>
+    </nav>
+    
+    <div class="container-fluid" style="margin-top: 80px;">
+        <h4>LEFT JOIN LOKOMOTIF DAN KURSI</h4>
+        <hr>
+        <table class="table" border="1" cellspacing="0" cellpadding="8">
+            <thead>
+                <th class="th">Kode Kereta</th>
+                <th class="th">Nama Kereta</th>
+                <th class="th">Kode Gerbong</th>
+                <th class="th">Kode Kursi</th>
+            </thead>
 
-        <tbody>
-            <tr>
-                <?php foreach ($left as $key)
-                {
+            <tbody>
+                <tr>
+                    <?php foreach ($left as $key)
+                    {
 
-                ?>
-                <td><?php echo $key->kd_keretaL ; ?></td>
-                <td><?php echo $key->nama ; ?></td>
-                <td><?php echo $key->kd_kursi ; ?></td>
-                <td><?php echo $key->kd_gerbong ; ?></td>
-                <td><?php echo $key->kd_keretaK ; ?></td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    <br><br>
-    <h4>RIGHT JOIN KURSI DAN GERBONG</h4>
-    <hr>
-    <table border="1" cellspacing="0" cellpadding="8">
-        <thead>
-            <th>kd_kursi(K)</th>
-            <th>kd_gerbong(K)</th>
-            <th>kd_kereta(K)</th>
-            <th>kd_gerbong(G)</th>
-            <th>kd_kereta(G)</th>
-            <th>nama(G)</th>
-        </thead>
+                    ?>
+                    <td><?php echo $key->kd_keretaL ; ?></td>
+                    <td><?php echo $key->nama ; ?></td>
+                    <td><?php echo $key->kd_gerbong ; ?></td>
+                    <td><?php echo $key->kd_kursi ; ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        <br><br>
+        <h4>RIGHT JOIN KURSI DAN GERBONG</h4>
+        <hr>
+        <table class="table" border="1" cellspacing="0" cellpadding="8">
+            <thead>
+                <th class="th">Kode Kursi</th>
+                <th class="th">Kode Gerbong</th>
+                <th class="th">Kode Kereta</th>
+                <th class="th">Nama Gerbong</th>
+            </thead>
 
-        <tbody>
-            <tr>
-                <?php foreach ($right as $key)
-                {
+            <tbody>
+                <tr>
+                    <?php foreach ($right as $key)
+                    {
 
-                ?>
-                <td><?php echo $key->kd_kursi ; ?></td>
-                <td><?php echo $key->kd_gerbongK ; ?></td>
-                <td><?php echo $key->kd_keretaK ; ?></td>
-                <td><?php echo $key->kd_gerbongG ; ?></td>
-                <td><?php echo $key->kd_keretaG ; ?></td>
-                <td><?php echo $key->nama ; ?></td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    <br><br>
-    <h4>INNER JOIN LOKOMOTIF, KURSI, DAN GERBONG</h4>
-    <hr>
-    <table border="1" cellspacing="0" cellpadding="8">
-        <thead>
-            <th>kd_kereta(L)</th>
-            <th>nama(L)</th>
-            <th>kd_kursi(K)</th>
-            <th>kd_gerbong(K)</th>
-            <th>kd_kereta(K)</th>
-            <th>kd_gerbong(G)</th>
-            <th>kd_kereta(G)</th>
-            <th>nama(G)</th>
-        </thead>
-        </thead>
+                    ?>
+                    <td><?php echo $key->kd_kursi ; ?></td>
+                    <td><?php echo $key->kd_gerbongG ; ?></td>
+                    <td><?php echo $key->kd_keretaG ; ?></td>
+                    <td><?php echo $key->nama ; ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        <br><br>
+        <h4>INNER JOIN LOKOMOTIF, KURSI, DAN GERBONG</h4>
+        <hr>
+        <table class="table" border="1" cellspacing="0" cellpadding="8">
+            <thead>
+                <th class="th">Kode Kereta</th>
+                <th class="th">Nama Kereta</th>
+                <th class="th">Kode Gerbong</th>
+                <th class="th">Nama Gerbong</th>
+                <th class="th">Kode Kursi</th>
+            </thead>
+            </thead>
 
-        <tbody>
-            <tr>
-                <?php foreach ($inner as $key)
-                {
+            <tbody>
+                <tr>
+                    <?php foreach ($inner as $key)
+                    {
 
-                ?>
-                <td><?php echo $key->kd_keretaL ; ?></td>
-                <td><?php echo $key->namaL ; ?></td>
-                <td><?php echo $key->kd_kursi ; ?></td>
-                <td><?php echo $key->kd_gerbongK ; ?></td>
-                <td><?php echo $key->kd_keretaK ; ?></td>
-                <td><?php echo $key->kd_gerbongG ; ?></td>
-                <td><?php echo $key->kd_keretaG ; ?></td>
-                <td><?php echo $key->namaG ; ?></td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
+                    ?>
+                    <td><?php echo $key->kd_keretaL ; ?></td>
+                    <td><?php echo $key->namaL ; ?></td>
+                    <td><?php echo $key->kd_gerbongK ; ?></td>
+                    <td><?php echo $key->namaG ; ?></td>
+                    <td><?php echo $key->kd_kursi ; ?></td>
+                </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        </div>
+
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-bottom"></nav>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
